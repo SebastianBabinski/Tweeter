e2bd0b6dc455f349041a6bd854cce95bc103645c
SebastianBabinski <babinski.sebastian@gmail.com>
2021-11-05 21:26:55 +0100

    Initial commit

---
+++ src/main/java/babinski/sebastian/Tweeter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package babinski.sebastian.Tweeter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TweeterApplication.class);
+	}
+
+}
