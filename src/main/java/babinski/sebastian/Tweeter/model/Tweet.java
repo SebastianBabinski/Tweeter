086be0982ee698c42bb218f234a19dbb3151ec8a
SebastianBabinski <babinski.sebastian@gmail.com>
2021-11-10 20:32:01 +0100

    added methods in service, controller class and also added swagger

--- src/main/java/babinski/sebastian/Tweeter/model/Tweet.java
+++ src/main/java/babinski/sebastian/Tweeter/model/Tweet.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class Tweet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String title;
     private String content;
